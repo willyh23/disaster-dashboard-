580dfde32f12e4de2f73dee81646292844a31caf
Willy Hung <willyh1223@gmail.com>
2025-10-31 23:41:27 -0700

    moved covert_wildfires.py to data folder

---
+++ data/convert_wildfires.py
@@ -0,0 +1,32 @@
+import csv
+import json
+
+features = []
+
+with open('wildfires_global.txt', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        features.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [float(row['longitude']), float(row['latitude'])]
+            },
+            "properties": {
+                "brightness": float(row['bright_ti4']),
+                "confidence": row['confidence'],
+                "frp": float(row['frp']),
+                "acq_date": row['acq_date'],
+                "acq_time": row['acq_time'],
+                "satellite": row['satellite'],
+                "daynight": row['daynight']
+            }
+        })
+
+geojson = {
+    "type": "FeatureCollection",
+    "features": features
+}
+
+with open('wildfires.geojson', 'w') as f:
+    json.dump(geojson, f, indent=2)
